dfa9bebda52b2d9a76c13071d9c6c6f408591dfb
Pujitha <pujitha.munjeti@gmail.com>
2021-03-12 18:35:34 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AgeCalculator';
+  dob;
+  Year=null;
+  Months;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(dob){
+    const date1=new Date(dob)
+    const date2=new Date();
+
+   
+  this.Year=date2.getFullYear()-date1.getFullYear();
+  
+  this.Months=date2.getMonth()-date1.getMonth();
+  if(this.Months<0){
+    this.Months=this.Months+12;
+    this.Year--;
+  }
+  
+  console.log("no of months are ",this.Months,this.Year)
+  
+  }
+
 }
